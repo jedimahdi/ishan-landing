5721870ae09e729e185bad83ca99c91ac1006078
Mahdi Seyedan <seyedmahdiseydan78@yahoo.com>
2020-01-30 11:14:45 +0330

    Updates

---
+++ precache-manifest.7c185aa891c8798187051af9c5496f48.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0733fccd95f5f645a10fe903cffedd",
+    "url": "/ishan-landing/index.html"
+  },
+  {
+    "revision": "5ee4a04dcf13a740601c",
+    "url": "/ishan-landing/static/css/main.4d4212e3.chunk.css"
+  },
+  {
+    "revision": "899ebb333d3b5d5e6f64",
+    "url": "/ishan-landing/static/js/2.6dbd53ec.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/ishan-landing/static/js/2.6dbd53ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ee4a04dcf13a740601c",
+    "url": "/ishan-landing/static/js/main.f341080f.chunk.js"
+  },
+  {
+    "revision": "661ef318064d55abb0bb",
+    "url": "/ishan-landing/static/js/runtime-main.4fc3a4e5.js"
+  },
+  {
+    "revision": "ce7fe3a21a202e932fc0117dd22418fb",
+    "url": "/ishan-landing/static/media/021.ce7fe3a2.jpg"
+  },
+  {
+    "revision": "3e7b29a87b8c8e457d0ad3efe576493b",
+    "url": "/ishan-landing/static/media/031.3e7b29a8.jpg"
+  },
+  {
+    "revision": "91cd141a2b488ca098716a795f018d39",
+    "url": "/ishan-landing/static/media/05.91cd141a.jpg"
+  },
+  {
+    "revision": "b0f36712bf650e7e8e5ef267e8f16fab",
+    "url": "/ishan-landing/static/media/1.b0f36712.jpg"
+  },
+  {
+    "revision": "e247c6cb9f01f8b1bc6ee73c12d9ea52",
+    "url": "/ishan-landing/static/media/DIGITALY.e247c6cb.jpg"
+  },
+  {
+    "revision": "099b205bbf66473540fad68330a603ed",
+    "url": "/ishan-landing/static/media/ETESAL-BAZARKAR.099b205b.jpg"
+  },
+  {
+    "revision": "61708455694373f25e75eef4c8252fbe",
+    "url": "/ishan-landing/static/media/FARAHANI.61708455.jpg"
+  },
+  {
+    "revision": "608618733cfeb4a7d743f2a52d687739",
+    "url": "/ishan-landing/static/media/GAME.60861873.jpg"
+  },
+  {
+    "revision": "a8f7b34257aae741202f7bb4a215dcc9",
+    "url": "/ishan-landing/static/media/GHAVANINHA.a8f7b342.jpg"
+  },
+  {
+    "revision": "e1725cbaa3c5f93efed15dd291a60725",
+    "url": "/ishan-landing/static/media/KARVARZI.e1725cba.jpg"
+  },
+  {
+    "revision": "b156eb9282ae2c3d78b9477dd24bff33",
+    "url": "/ishan-landing/static/media/MOSHAVEEH-HEADER.b156eb92.jpg"
+  },
+  {
+    "revision": "a4efc4a24e0ff778f4fb4bb8cb2ea031",
+    "url": "/ishan-landing/static/media/PROGRAMING.a4efc4a2.jpg"
+  },
+  {
+    "revision": "3140a320953ce8569a5276a9774ff6b2",
+    "url": "/ishan-landing/static/media/RENDER.3140a320.jpg"
+  },
+  {
+    "revision": "f31d6668743c55159a75b519f7fdca88",
+    "url": "/ishan-landing/static/media/RIG.f31d6668.jpg"
+  },
+  {
+    "revision": "ebe48b77190deb47af2c2940ade25823",
+    "url": "/ishan-landing/static/media/animat.ebe48b77.jpg"
+  },
+  {
+    "revision": "bf1f2ac35f43407c4a05e4840005a08c",
+    "url": "/ishan-landing/static/media/asatid.bf1f2ac3.png"
+  },
+  {
+    "revision": "1b14d16fb63e0db8feb3b28e521fee06",
+    "url": "/ishan-landing/static/media/django.1b14d16f.jpg"
+  },
+  {
+    "revision": "6e77b1774bd4502d6eb6b8084e4adacb",
+    "url": "/ishan-landing/static/media/ertebatbama.6e77b177.jpg"
+  },
+  {
+    "revision": "482907c35012f4e2041cf3075ed35bb1",
+    "url": "/ishan-landing/static/media/eshteraki.482907c3.jpg"
+  },
+  {
+    "revision": "d387576f604baf8444d10dcc5c9fa6ac",
+    "url": "/ishan-landing/static/media/fani-HEADER.d387576f.jpg"
+  },
+  {
+    "revision": "d387576f604baf8444d10dcc5c9fa6ac",
+    "url": "/ishan-landing/static/media/fani.d387576f.jpg"
+  },
+  {
+    "revision": "f199b945534d0c1514524ad925b47068",
+    "url": "/ishan-landing/static/media/faqq.f199b945.jpg"
+  },
+  {
+    "revision": "93bd6d74ad6d395986d3afa301818537",
+    "url": "/ishan-landing/static/media/ghavanin.93bd6d74.jpg"
+  },
+  {
+    "revision": "2d7cc7cd97d682b6ca62d2defc76dc5c",
+    "url": "/ishan-landing/static/media/iranyekanwebbold.2d7cc7cd.woff"
+  },
+  {
+    "revision": "7c912e00bad2995b662cf8db5396357a",
+    "url": "/ishan-landing/static/media/iranyekanwebbold.7c912e00.eot"
+  },
+  {
+    "revision": "a7d41f91c7dd7fe1f24947149e33b6c2",
+    "url": "/ishan-landing/static/media/iranyekanwebbold.a7d41f91.ttf"
+  },
+  {
+    "revision": "fe12f13a160a0abadd60db289937a1ef",
+    "url": "/ishan-landing/static/media/iranyekanwebbold.fe12f13a.woff2"
+  },
+  {
+    "revision": "088dfe8a992231933d93128e54ae7da4",
+    "url": "/ishan-landing/static/media/iranyekanweblight.088dfe8a.woff"
+  },
+  {
+    "revision": "2c83275a16506746b3cbaf0c2e3eea68",
+    "url": "/ishan-landing/static/media/iranyekanweblight.2c83275a.ttf"
+  },
+  {
+    "revision": "699085bdade384f68b8206f10fcbd914",
+    "url": "/ishan-landing/static/media/iranyekanweblight.699085bd.eot"
+  },
+  {
+    "revision": "a80f71ff899a3f07228e1584b6426744",
+    "url": "/ishan-landing/static/media/iranyekanweblight.a80f71ff.woff2"
+  },
+  {
+    "revision": "1fdcadecb1a82e89e6d2547bc027976e",
+    "url": "/ishan-landing/static/media/iranyekanwebregular.1fdcadec.woff2"
+  },
+  {
+    "revision": "56a1baa4ead5aeac939b5dc2d94df837",
+    "url": "/ishan-landing/static/media/iranyekanwebregular.56a1baa4.woff"
+  },
+  {
+    "revision": "5bea6c32a95de7f8be577a95d65a85cc",
+    "url": "/ishan-landing/static/media/iranyekanwebregular.5bea6c32.eot"
+  },
+  {
+    "revision": "79533773cd43a707a69d89d5f4d07e85",
+    "url": "/ishan-landing/static/media/iranyekanwebregular.79533773.ttf"
+  },
+  {
+    "revision": "5edc4ef3a11da03658767f1303af658e",
+    "url": "/ishan-landing/static/media/logo.5edc4ef3.svg"
+  },
+  {
+    "revision": "60968915263aefbf52f7409cf1d9dc91",
+    "url": "/ishan-landing/static/media/mentoring.60968915.jpg"
+  },
+  {
+    "revision": "7d047b5e915131d254823b84317abafe",
+    "url": "/ishan-landing/static/media/reshtehaa.7d047b5e.jpg"
+  },
+  {
+    "revision": "cfa62e23604e1648ed01bd55358703dd",
+    "url": "/ishan-landing/static/media/sabtenam-HEADER.cfa62e23.jpg"
+  },
+  {
+    "revision": "84658117dafe2196252e9632baecf2ca",
+    "url": "/ishan-landing/static/media/sabtenam.84658117.jpg"
+  },
+  {
+    "revision": "ade340817ab18a819d91ca6ebe591e07",
+    "url": "/ishan-landing/static/media/shahriyeh-HEADER.ade34081.jpg"
+  },
+  {
+    "revision": "21c3dbeac41d3d47cf0fb4e8c70ffdc8",
+    "url": "/ishan-landing/static/media/shahriyeh.21c3dbea.jpg"
+  },
+  {
+    "revision": "ee8d133db4fd2e6f4581e36581b4f857",
+    "url": "/ishan-landing/static/media/shetabdahandeh-HEDEAR.ee8d133d.jpg"
+  },
+  {
+    "revision": "6d657e1813a81bce2a6955ccb4b3a38d",
+    "url": "/ishan-landing/static/media/techerssssss.6d657e18.jpg"
+  },
+  {
+    "revision": "cb6498bde3d15afd9ab45f3fed071935",
+    "url": "/ishan-landing/static/media/texturing.cb6498bd.jpg"
+  },
+  {
+    "revision": "f2ec62e9807f715cb8a7013a09ae8f04",
+    "url": "/ishan-landing/static/media/vfx.f2ec62e9.jpg"
+  }
+]);
