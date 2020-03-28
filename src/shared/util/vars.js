61e77dceb93f39608fd99e20f32ef1752f472268
Mahdi SE <seyedmahdiseydan78@yahoo.com>
2020-03-28 13:48:58 +0430

    back to http

--- src/shared/util/vars.js
+++ src/shared/util/vars.js
@@ -3,8 +3,8 @@ let api_url
 // if (process.env.NODE_ENV === 'development') {
 //   api_url = 'http://localhost:3000/';
 // } else {
-api_url = 'https://app.i-shan.ir/api/'
+api_url = 'http://app.i-shan.ir/api/'
 // }
 
 export const API_BASE_URL = api_url
-export const SERVER_URL = 'https://app.i-shan.ir/'
+export const SERVER_URL = 'http://app.i-shan.ir/'
